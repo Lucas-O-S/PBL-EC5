6608968ee5e94ccd9bc224ca5ca5008ea4f6b126
Lucas-O-S <lucasoscomercial@gmail.com>
2024-09-18 13:05:16 -0300

    Criação do projeto

---
+++ Programa/SitePBL/SitePBL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SitePBL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
