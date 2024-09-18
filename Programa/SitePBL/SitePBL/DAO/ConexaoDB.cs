1fed3cc11324735f173a6597abdd1f3284aba20e
Lucas-O-S <lucasoscomercial@gmail.com>
2024-09-18 13:14:06 -0300

    Tela de erro, conexão DB e Helper DAO criados

---
+++ Programa/SitePBL/SitePBL/DAO/ConexaoDB.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SqlClient;
+
+namespace SitePBL.DAO
+{
+    public class ConexaoDB
+    {
+        public static SqlConnection GetConexao()
+        {
+            string strCon = "Data Source=LOCALHOST;  Database=ColocarNome; user id=SA; password=123456";
+            SqlConnection conexao = new SqlConnection(strCon);
+            conexao.Open();
+            return conexao;
+        }
+    }
+}
