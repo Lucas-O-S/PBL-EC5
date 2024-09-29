7fc0968cb52b30dd53e4d9042a6175de61a144a1
Lucas-O-S <lucasoscomercial@gmail.com>
2024-09-29 20:45:11 -0300

    Arrumando listagem de empresas

--- Programa/SitePBL/SitePBL/obj/Release/net6.0/SitePBL.AssemblyInfo.cs
+++ Programa/SitePBL/SitePBL/obj/Release/net6.0/SitePBL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SitePBL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2505a9da19e05e717c81b9fcd4681b515ee47c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SitePBL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SitePBL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
