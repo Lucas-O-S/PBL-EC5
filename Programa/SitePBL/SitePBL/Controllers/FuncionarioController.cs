21674656c48c10bfe90df4fc9e4d2df62d5502bf
ROBSON GUILHERME FERRAREZI <g.robsonferrarezi@gmail.com>
2024-11-12 20:40:51 -0300

    Alterado o Funcionario.Form e o Funcionario Controller

--- Programa/SitePBL/SitePBL/Controllers/FuncionarioController.cs
+++ Programa/SitePBL/SitePBL/Controllers/FuncionarioController.cs
@@ -11,8 +11,13 @@ namespace SitePBL.Controllers
         protected override void ValidarDados(FuncionarioViewModel model, string operacao)
         {
             base.ValidarDados(model, operacao);
+            if (string.IsNullOrEmpty(model.nome))
+                ModelState.AddModelError("nome", "Campo obrigatório! Preencha o nome!");
+            if (string.IsNullOrEmpty(model.cargo))
+                ModelState.AddModelError("cargo", "Campo obrigatório! Preencha o cargo!");
+            if (model.dataContratacao > DateTime.Now || model.dataContratacao < new DateTime(1900, 1, 1))
+                ModelState.AddModelError("dataContratacao", "Digite uma data válida!");
         }
-
 
     }
 }
